5917e3112eb370077268c7992c6626ede2beaeed
Safonov85 <safonov85@yahoo.com>
2017-01-26 20:54:26 +0100

    First Commit

---
+++ TemptraturRegister/Main.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TemptraturRegister
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(373, 117);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(121, 277);
+            this.listBox.TabIndex = 1;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(76, 117);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(257, 277);
+            this.textBox.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 475);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.listBox);
+            this.Name = "Main";
+            this.Text = "Temp Reg";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ TemptraturRegister/Main.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace TemptraturRegister
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //using (TextWriter tw = new StreamWriter("TempInfo//SavedList.txt"))
+            //{
+            //    foreach (String s in IList.verbList)
+            //    {
+            //        tw.WriteLine(s);
+
+            //    }
+            //}
+        }
+
+        void GenerateNumbers()
+        {
+            int num = 0;
+            List<int> list = new List<int>();
+
+            while (num < 10)
+            {
+                list.Add(num);
+                //listViewItem.add
+                //this.Text = num.ToString();
+                num++;
+            }
+            foreach (int number in list)
+            {
+                //listView.Items.Add(number.ToString());
+                listBox.Items.Add(number.ToString());
+            }
+
+            //listBox.Show();
+            //listView.Show();
+        }
+
+        public void GetTableInfo(string html)
+        {
+            //Console.WriteLine("Downloading and Loading data...");
+
+            string htmlCode = "";
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.UserAgent, "AvoidError");
+                htmlCode = client.DownloadString(html);
+            }
+            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+
+            document.LoadHtml(htmlCode);
+
+            foreach (HtmlNode node in document.DocumentNode.SelectNodes("//td[@class='weather current']"))
+            {
+                string currentTempValue;
+                //if (node.Id == "current-conditions-handle")
+                //{
+                //    this.Text = "found it";
+                //}
+                //listBox.Items.Add(textBox.Text);
+                if (node.InnerText.Contains("º"))
+                {
+                    currentTempValue = node.InnerText;
+                    string removeTabs = currentTempValue.Replace("\t", "");
+                    string removeSpaces = removeTabs.Replace("\n", "");
+                    string getTemp = removeSpaces.Substring(0, 2);
+                    //currentTempValue.Replace(" ", string.Empty);
+                    //string getTempNumber = new String(currentTempValue.TakeWhile(Char.IsDigit).ToArray());
+                    textBox.Text = getTemp;
+                    this.Text = getTemp;
+                    break;
+                }
+
+                //foreach (HtmlNode node2 in node.SelectNodes("//div"))
+                //{
+                //    if (node.InnerText.Contains("º"))
+                //    {
+                //        textBox.Text = node2.InnerText;
+                //    }
+                //}
+
+                //foreach (HtmlNode div in document.DocumentNode.SelectNodes("//div"))
+                //{
+                //    textBox.Text = div.InnerText;
+                //}
+
+                //if (table.Id == "sendungsHistorieOne")
+                //{
+                //    string innerText = table.InnerText.Replace("&nbsp;", "");
+
+                //    Console.WriteLine(innerText);
+                //}
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            GenerateNumbers();
+            GetTableInfo("http://www.klart.se/väder-björsäter.html");
+        }
+    }
+}
